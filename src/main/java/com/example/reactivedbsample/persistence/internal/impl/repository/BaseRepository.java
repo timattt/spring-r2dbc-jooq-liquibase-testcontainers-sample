c5631be009389f6c626d647d6f03db2c21fec1f8
timattt <timattt@gmail.com>
2025-05-13 12:43:44 +0300

    SOLUTION!

---
+++ src/main/java/com/example/reactivedbsample/persistence/internal/impl/repository/BaseRepository.java
@@ -0,0 +1,45 @@
+package com.example.reactivedbsample.persistence.internal.impl.repository;
+
+import io.r2dbc.pool.ConnectionPool;
+import org.jooq.DSLContext;
+import org.jooq.Publisher;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.r2dbc.connection.ConnectionHolder;
+import org.springframework.transaction.reactive.TransactionContext;
+import reactor.core.publisher.Mono;
+import reactor.util.context.ContextView;
+
+import java.util.function.Function;
+
+public abstract class BaseRepository {
+    @Autowired
+    private ConnectionPool connectionPool;
+    @Autowired
+    private DSLContext defaultDslContext;
+
+    protected <T> Mono<T> dslMono(Function<DSLContext, Mono<T>> function) {
+        return Mono.deferContextual(ctx -> {
+            DSLContext dsl = extractDsl(ctx);
+            return function.apply(dsl);
+        });
+    }
+
+    protected <T> Mono<T> dsl(Function<DSLContext, Publisher<T>> function) {
+        return Mono.deferContextual(ctx -> {
+            DSLContext dsl = extractDsl(ctx);
+            return Mono.from(function.apply(dsl));
+        });
+    }
+
+    private DSLContext extractDsl(ContextView ctx) {
+        try {
+            TransactionContext context = ctx.getOrDefault(TransactionContext.class, null);
+            ConnectionHolder holder = (ConnectionHolder) context.getResources().get(connectionPool);
+            return DSL.using(holder.getConnection(), SQLDialect.POSTGRES);
+        } catch (Exception ignored) {
+            return defaultDslContext;
+        }
+    }
+}
--- src/main/java/com/example/reactivedbsample/persistence/internal/impl/repository/UserRepositoryImpl.java
+++ src/main/java/com/example/reactivedbsample/persistence/internal/impl/repository/UserRepositoryImpl.java
@@ -3,36 +3,31 @@ package com.example.reactivedbsample.persistence.internal.impl.repository;
 import com.example.reactivedbsample.bl.domain.api.model.ExampleUser;
 import com.example.reactivedbsample.bl.operation.spi.repository.UserRepository;
 import com.example.reactivedbsample.generated.Tables;
-import lombok.RequiredArgsConstructor;
-import org.jooq.DSLContext;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
-@RequiredArgsConstructor
-public class UserRepositoryImpl implements UserRepository {
-    final DSLContext dsl;
-
+public class UserRepositoryImpl extends BaseRepository implements UserRepository {
     @Override
     public Mono<ExampleUser> loadByEmailAndPessimisticallyLock(String email) {
-        return Mono.from(dsl
+        return dsl(dsl -> dsl
                         .selectFrom(Tables.EXAMPLE_USER)
                         .where(Tables.EXAMPLE_USER.EMAIL.eq(email))
                         .forUpdate())
-                .map(r -> r.into(ExampleUser.class));
+            .map(r -> r.into(ExampleUser.class));
     }
 
     @Override
     public Mono<ExampleUser> save(ExampleUser user) {
         if (user.id() == 0) {
-            return Mono.from(dsl
+            return dsl(dsl -> dsl
                             .insertInto(Tables.EXAMPLE_USER)
                             .set(Tables.EXAMPLE_USER.EMAIL, user.email())
                             .set(Tables.EXAMPLE_USER.PASSWORD, user.password())
                             .returningResult(Tables.EXAMPLE_USER.USER_ID))
                     .map(r -> new ExampleUser(r.value1(), user.email(), user.password()));
         }
-        return Mono.from(dsl
+        return dsl(dsl -> dsl
                 .update(Tables.EXAMPLE_USER)
                 .set(Tables.EXAMPLE_USER.EMAIL, user.email())
                 .set(Tables.EXAMPLE_USER.PASSWORD, user.password())
